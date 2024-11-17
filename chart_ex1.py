f8ec47b44bc3a1377b79843995096baab18b5081
Duger83 <dugurov@gmail.com>
2024-11-17 22:06:43 +0400

    Задача_1_графики

--- chart_ex1.py
+++ chart_ex1.py
@@ -9,20 +9,20 @@ def mathplot_chart(mass):
     plt.grid()
     plt.xlabel('ось Х')
     plt.ylabel('ось У') 
-    plt.title('y=cos(20x)/(x+0.1)')
+    plt.title('y=cos(20x)/(x+0.1) by mathplotlib')
     plt.show()
 
 def sea_chart(mass): 
     sns.set_style("ticks",{'axes.grid' : True})
     df = pd.DataFrame({'ось X': np.arange(0, 4.01, 0.01), 'ось Y': mass})
     sns.lineplot(df, color='red', x='ось X', y='ось Y')
-    plt.title('y=cos(20x)/(x+0.1)')
+    plt.title('y=cos(20x)/(x+0.1) by seaborn')
     plt.show()
 
 def plotly_chart(mass):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=np.arange(0, 4.01, 0.01), y=mass, line=dict(color='Green')))
-    fig.update_layout(title='y=cos(20x)/(x+0.1)', xaxis_title='ось X', yaxis_title='ось У')
+    fig.update_layout(title='y=cos(20x)/(x+0.1) by plotly', xaxis_title='ось X', yaxis_title='ось У')
     fig.show()
 
 a = np.array([np.cos(20*x)/(x+0.1) for x in np.arange(0, 4.01, 0.01)])
